5d43bcaf833786adfeb90629c0354c58f1a756e6
karol <karol.kondraciuk@uj.edu.pl>
2017-07-11 14:42:45 +0200

    cleanup

---
+++ tests/src/RouterTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace FitdevPro\FitRouter\Tests;
+
+use FitdevPro\FitRouter\Router;
+use FitdevPro\FitRouter\TestsLib\FitTest;
+
+class RouterTest extends FitTest
+{
+    public function testRouter()
+    {
+        $router = new Router();
+    }
+}
