10679b083e1a6af07e7332c75d451df20d53d61b
karol <karol.kondraciuk@uj.edu.pl>
2017-07-14 13:26:46 +0200

    url filler tests

--- tests/src/UrlFillers/ArgsWithLangFillerTest.php
+++ tests/src/UrlFillers/ArgsWithLangFillerTest.php
@@ -4,50 +4,50 @@ namespace FitdevPro\FitRouter\Tests\UrlFillers;
 
 use FitdevPro\FitRouter\Route;
 use FitdevPro\FitRouter\TestsLib\FitTest;
-use FitdevPro\FitRouter\UrlFillers\ArgsFiller;
+use FitdevPro\FitRouter\UrlFillers\ArgsWithLangFiller;
 
-class ArgsFillerTest extends FitTest
+class ArgsWithLangFillerTest extends FitTest
 {
     public function testFillNoParams()
     {
         $route = $this->prophesize(Route::class);
         $route->getUrl()->willReturn('test');
-        $urlFiller = new ArgsFiller();
+        $urlFiller = new ArgsWithLangFiller('en');
 
-        $this->assertEquals('test', $urlFiller->getUrl($route->reveal(), []));
+        $this->assertEquals('/en/test', $urlFiller->getUrl($route->reveal(), []));
     }
 
     public function testFill()
     {
         $route = $this->prophesize(Route::class);
         $route->getUrl()->willReturn('test/:name');
-        $urlFiller = new ArgsFiller();
+        $urlFiller = new ArgsWithLangFiller('en');
 
-        $this->assertEquals('test/bazz', $urlFiller->getUrl($route->reveal(), ['name' => 'bazz']));
+        $this->assertEquals('/en/test/bazz', $urlFiller->getUrl($route->reveal(), ['name' => 'bazz']));
     }
 
     /**
      * @expectedException \FitdevPro\FitRouter\Exception\UrlFillerException
-     * @expectedExceptionCode 1815010601
+     * @expectedExceptionCode 1815120601
      */
     public function testFillTooFewArgs()
     {
         $route = $this->prophesize(Route::class);
         $route->getUrl()->willReturn('test/:name/:id');
-        $urlFiller = new ArgsFiller();
+        $urlFiller = new ArgsWithLangFiller('en');
 
         $this->assertEquals('test/bazz', $urlFiller->getUrl($route->reveal(), ['name' => 'bazz']));
     }
 
     /**
      * @expectedException \FitdevPro\FitRouter\Exception\UrlFillerException
-     * @expectedExceptionCode 1815010602
+     * @expectedExceptionCode 1815120602
      */
     public function testFillBadArgs()
     {
         $route = $this->prophesize(Route::class);
         $route->getUrl()->willReturn('test/:name/:id');
-        $urlFiller = new ArgsFiller();
+        $urlFiller = new ArgsWithLangFiller('en');
 
         $this->assertEquals('test/bazz', $urlFiller->getUrl($route->reveal(), ['name' => 'bazz', 'di' => 1]));
     }
